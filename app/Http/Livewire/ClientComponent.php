0940b73c15dad59232a8a2e1a3bf251d8b49f3a1
SonJiren <jirenthegreyuniver11@gmail.com>
2024-02-15 13:43:07 -0600

    logrado la tabla de cliente

--- app/Http/Livewire/ClientComponent.php
+++ app/Http/Livewire/ClientComponent.php
@@ -17,13 +17,31 @@ class ClientComponent extends Component
     public $city;
     public $home;
     public $client_id;
+    public $selectedCountry;
+    public $searchCity = '';
+    public $client;
+
+    public function editClient($clientId)
+{
+    $this->resetValidation();
+    $this->resetFields();
+
+    $client = Client::findOrFail($clientId);
+    $this->client_id = $clientId;
+    $this->name = $client->name;
+    $this->phone = $client->phone;
+    $this->selectedCountry = $client->country;
+    $this->city = $client->city;
+    $this->home = $client->home;
+
+}
 
     protected $rules = [
-        'name' => 'required|max:255',
-        'phone' => 'required|max:255',
-        'country' => 'required|max:255',
-        'city' => 'required|max:255',
-        'home' => 'required|max:255',
+        'name' => 'required|max:30',
+        'phone' => 'required|max:10',
+        'country' => 'required|max:40',
+        'city' => 'required|max:40',
+        'home' => 'required|max:50',
     ];
 
     public function updated($propertyName)
@@ -31,23 +49,40 @@ class ClientComponent extends Component
         $this->validateOnly($propertyName);
     }
 
-
-
     public function render()
     {
         $this->clients = Client::all();
-        return view('livewire.client-component');
+        return view('livewire.client-component',[
+            'countries' => ['Mexico', 'Canadá', 'Estados Unidos'],
+            'cities' => $this->filterCities(),
+        ]);
     }
 
+    public function filterCities()
+    {
+        $filteredCities = [];
+        $allCities = $this->getCities();
+
+        foreach ($allCities as $city) {
+            if (stripos($city, $this->searchCity) !== false) {
+                $filteredCities[] = $city;
+            }
+        }
+
+        return $filteredCities;
+    }
     public function addClient()
     {
-        $this->validate();
+/*         $this->validate();
+ */
+        $this->country = $this->selectedCountry;
+        $this->city = $this->city;
 
         Client::create([
             'name' => $this->name,
             'phone' => $this->phone,
-            'country' => $this->country,
-            'city' => $this->city,
+            'country' =>$this->selectedCountry,
+            'city' =>$this->city,
             'home' => $this->home,
         ]);
 
@@ -71,4 +106,70 @@ class ClientComponent extends Component
         $this->confirmDeleteClientModal = false;
         $this->resetFields();
     }
+
+    public function openEditModal($client_id)
+    {
+        $this->client_id = $client_id;
+        $client = Client::find($client_id);
+        $this->name = $client->name;
+        $this->phone = $client->phone;
+        $this->country = $client->country;
+        $this->city = $client->city;
+        $this->home = $client->home;
+
+        $this->clientModal = true;
+    }
+
+    public function updateClient()
+    {
+        $this->validate();
+
+        $client = Client::find($this->client_id);
+        $client->update([
+            'name' => $this->name,
+            'phone' => $this->phone,
+            'country' => $this->selectedCountry,
+            'country' => $this->country,
+            'city' => $this->city,
+            'home' => $this->home,
+        ]);
+
+        $this->resetFields();
+
+        $this->clientModal = false;
+    }
+
+    public function confirmDeleteService($client_id)
+    {
+        $this->client_id = $client_id;
+        $this->confirmDeleteClientModal = true;
+    }
+
+    public function deleteClient()
+    {
+        $client = Client::find($this->client_id);
+        $client->delete();
+
+        $this->confirmDeleteClientModal = false;
+    }
+
+    public function getCities()
+    {
+        if ($this->selectedCountry === 'Mexico') {
+            return ['Ciudad de México', 'Guadalajara', 'Monterrey', 'Puebla', 'Tijuana', 'Matamoros','Reynosa','Ciudad Juárez','Tampico','Altamira','Ciudad Victoria', 'León', 'Zapopan', 'Mérida', 'San Luis Potosí', 'Aguascalientes', 'Hermosillo', 'Saltillo', 'Mexicali', 'Culiacán', 'Cancún', 'Chihuahua', 'Durango', 'Toluca', 'Querétaro', 'Morelia', 'Tuxtla Gutiérrez', 'Tlaxcala', 'Zacatecas', 'Victoria', 'Colima', 'La Paz', 'Cuernavaca', 'Campeche', 'Chilpancingo', 'Hidalgo', 'Oaxaca', 'Tabasco', 'Nayarit', 'Sonora', 'Yucatán', 'Chiapas', 'Guanajuato', 'Veracruz', 'Sinaloa', 'Jalisco', 'Nuevo León', 'Coahuila', 'Quintana Roo', 'Tamaulipas', 'San Luis Potosí', 'Aguascalientes', 'Durango', 'Zacatecas', 'Querétaro', 'Hidalgo', 'Tlaxcala', 'Nayarit', 'Colima', 'Quintana Roo', 'Baja California Sur', 'Campeche', 'Sonora', 'Yucatán', 'Chiapas', 'Guanajuato', 'Veracruz', 'Sinaloa', 'Jalisco', 'Nuevo León', 'Coahuila', 'Michoacán'];
+        } elseif ($this->selectedCountry === 'Canadá') {
+            return ['Toronto', 'Montreal', 'Vancouver', 'Calgary', 'Ottawa', 'Edmonton', 'Winnipeg', 'Québec', 'Hamilton', 'Victoria', 'London', 'Kitchener', 'St. Catharines', 'Halifax', 'Oshawa', 'Windsor', 'Saskatoon', 'Regina', 'Sherbrooke', 'Barrie', 'Saint John', 'Kelowna', 'Abbotsford', 'Sudbury', 'Kingston', 'Trois-Rivières', 'Chicoutimi', 'Thunder Bay', 'Moncton', 'Brantford', 'Nanaimo', 'Fredericton', 'Saint John', 'Drummondville', 'Newmarket', 'Peterborough', 'Chilliwack', 'Red Deer', 'Prince George', 'Sault Ste. Marie', 'North Bay', 'Norfolk', 'Brampton', 'Saint-Jérôme', 'Granby', 'Red Deer', 'Peterborough', 'Belleville', 'Chatham-Kent', 'Saint-Hyacinthe', 'Lethbridge', 'Moose Jaw', 'Medicine Hat', 'Granby', 'Belleville', 'Saint-Hyacinthe', 'North Bay', 'Drummondville', 'Saint-Jérôme', 'Belleville', 'Drummondville', 'North Bay', 'Granby', 'Timmins', 'Rimouski', 'Prince Albert', 'Pembroke', 'Saint-Georges', 'Rouyn-Noranda', 'Owen Sound', 'Woodstock', 'Corner Brook', 'Leamington', 'St. Thomas', 'Courtenay', 'Campbell River', 'Stratford', 'Orillia', 'New Glasgow', 'Bradford West Gwillimbury', 'Grande Prairie', 'Terrace', 'Stratford', 'Orillia', 'Cobourg', 'Amos', 'Val-d\'Or', 'Baie-Comeau', 'Sept-Îles', 'Powell River', 'Sylvan Lake', 'Lloydminster', 'Kenora', 'Swift Current', 'Whitecourt', 'Estevan', 'Williams Lake', 'North Battleford', 'Wasaga Beach', 'Canmore', 'Dryden', 'Cochrane', 'Camrose', 'Innisfail', 'Wetaskiwin', 'Selkirk', 'Steinbach', 'Dauphin', 'The Pas', 'Flin Flon', 'Thompson', 'Norway House', 'Churchill', 'Yorkton', 'Melfort', 'Humboldt', 'La Ronge', 'Meadow Lake', 'Esterhazy', 'Kindersley', 'Nipawin', 'La Loche', 'Assiniboia', 'Carlyle', 'Weyburn', 'Moose Jaw', 'Swift Current', 'Estevan', 'Humboldt', 'Melville', 'Tisdale', 'Rosetown', 'Maple Creek', 'Shaunavon', 'Biggar', 'Watrous', 'Balgonie', 'Caronport', 'Eston', 'Grenfell', 'Indian Head', 'Kerrobert', 'Kipling', 'Langham', 'Lumsden', 'Manor', 'Maple Creek', 'Moosomin', 'Nokomis', 'Pilot Butte', 'Preeceville', 'Regina Beach', 'Rosthern', 'Shaunavon', 'Shellbrook', 'Unity', 'Wilkie', 'Wolseley', 'Wynyard', 'Carnduff', 'Creighton', 'Dalmeny', 'Duck Lake', 'Gull Lake', 'Lashburn', 'Macklin', 'Marsden', 'Porcupine Plain', 'Raymore', 'Redvers', 'Stoughton', 'Wadena', 'Watrous', 'Wolseley', 'Arcola', 'Balgonie', 'Bengough', 'Bredenbury', 'Burstall', 'Canora', 'Carrot River', 'Choiceland', 'Craik', 'Creelman', 'Cupar', 'Davidson', 'Dysart', 'Edenwold', 'Emerald Park', 'Esterhazy', 'Fleming', 'Foam Lake', 'Francis', 'Grenfell', 'Gull Lake', 'Hanley', 'Hepburn', 'Hudson Bay', 'Imperial', 'Indian Head', 'Ituna', 'Kamsack', 'Kelliher', 'Kelvington', 'Kenaston', 'Kerrobert', 'Kinistino', 'Kyle', 'Lampman', 'Lang', 'Lashburn', 'Lemberg', 'Leroy', 'Lumsden', 'Luseland', 'Maidstone', 'Maple Creek', 'Meacham', 'Milestone', 'Montmartre', 'Montrose', 'Mortlach', 'Mossbank', 'Naicam', 'Nipawin', 'Osler', 'Outlook', 'Oxbow', 'Pangman', 'Pense', 'Pierceland', 'Pilot Butte', 'Prince Albert', 'Radisson', 'Regina Beach', 'Rocanville', 'Rose Valley', 'Rosetown', 'Rosthern', 'Saltcoats', 'Saskatoon', 'Shaunavon', 'Shellbrook', 'Spiritwood', 'St. Louis', 'St. Walburg', 'Stewart Valley', 'Stockholm', 'Storthoaks', 'Stoughton', 'Strasbourg', 'Sturgis', 'Swift Current', 'Tisdale', 'Turtleford', 'Unity', 'Vanscoy', 'Vibank', 'Viscount', 'Wadena', 'Wakaw', 'Waldheim', 'Warman', 'Watrous', 'Watson', 'Wawota', 'Weyburn', 'White City', 'Wilkie', 'Willow Bunch', 'Windthorst', 'Wiseton', 'Wolseley', 'Wood Mountain', 'Yorkton', 'Young', 'Humboldt'];
+        } elseif ($this->selectedCountry === 'Estados Unidos') {
+            return ['Nueva York', 'Los Ángeles', 'Chicago', 'Houston', 'Phoenix', 'Filadelfia', 'San Antonio', 'San Diego', 'Dallas', 'San José', 'Austin', 'Indianapolis', 'Jacksonville', 'San Francisco', 'Columbus', 'Fort Worth', 'Charlotte', 'Detroit', 'El Paso', 'Memphis', 'Seattle', 'Denver', 'Washington D.C.', 'Boston', 'Nashville', 'Baltimore', 'Oklahoma City', 'Portland', 'Las Vegas', 'Milwaukee', 'Albuquerque', 'Tucson', 'Fresno', 'Sacramento', 'Mesa', 'Kansas City', 'Atlanta', 'Long Beach', 'Colorado Springs', 'Raleigh', 'Miami', 'Virginia Beach', 'Omaha', 'Oakland', 'Minneapolis', 'Tulsa', 'Wichita', 'New Orleans', 'Arlington', 'Cleveland', 'Bakersfield', 'Tampa', 'Aurora', 'Honolulu', 'Anaheim', 'Santa Ana', 'Corpus Christi', 'Riverside', 'Lexington', 'Stockton', 'St. Louis', 'Saint Paul', 'Henderson', 'Pittsburgh', 'Cincinnati', 'Anchorage', 'Greensboro', 'Plano', 'Newark', 'Lincoln', 'Toledo', 'Orlando', 'Chula Vista', 'Irvine', 'Fort Wayne', 'Jersey City', 'Durham', 'Laredo', 'Madison', 'Buffalo', 'Lubbock', 'Gilbert', 'Winston-Salem', 'Glendale', 'Hialeah', 'Garland', 'Scottsdale', 'Irving', 'Chesapeake', 'North Las Vegas', 'Fremont', 'Baton Rouge', 'Richmond', 'Boise', 'San Bernardino', 'Spokane', 'Montgomery', 'Des Moines', 'Modesto', 'Tacoma', 'Shreveport', 'Fontana', 'Oxnard', 'Aurora', 'Moreno Valley', 'Akron', 'Yonkers', 'Columbus', 'Augusta', 'Little Rock', 'Amarillo', 'Glendale', 'Huntington Beach', 'Grand Rapids', 'Mobile', 'Salt Lake City', 'Tallahassee'];
+        } else {
+            return [];
+        }
+
+    }
+
+    public function updatedSelectedCountry()
+    {
+        $this->reset(['city', 'searchCity']);
+    }
+
 }
--- resources/views/livewire/client-component.blade.php
+++ resources/views/livewire/client-component.blade.php
@@ -1,14 +1,14 @@
 
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
     <div class="mb-4">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center" wire:click="$toggle('serviceModal')">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center" wire:click="$toggle('clientModal')">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
             </svg>
             Crear Cliente
         </button>
             </div>
-    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
 
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -26,8 +26,9 @@
                                 <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                                     <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $client->name }}</td>
                                     <td class="px-6 py-4">{{ $client->phone }}</td>
-                                    <td class="px-6 py-4">{{ $client->country }}, {{ $client->city }}, {{ $client->home }}</td>
-                                    <td class="px-6 py-4">{{ $client->date }}</td>
+                                    <td class="px-6 py-4">{{ $client->country }}</td>
+                                    <td class="px-6 py-4"> {{ $client->city }}</td>
+                                    <td class="px-6 py-4"> {{ $client->home }}</td>
                                     <td class="px-6 py-4">
                                         <button wire:click="openEditModal({{ $client->id }})" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded flex items-center">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
@@ -47,9 +48,107 @@
                                 @endforeach
                             </tbody>
                         </table>
-                    </div>
-                </div>
-            </div>
+                        <x-dialog-modal wire:model="clientModal">
+                            <x-slot name="title">{{ !$client_id ? 'Nuevo cliente' : 'Editar cliente' }}</x-slot>
+                            <x-slot name="content">
+                                <div class="space-y-4">
+                                    <div>
+                                        <x-label>Nombre</x-label>
+                                        <x-input wire:model="name" class="w-full" />
+                                        @error('name')
+                                            <span>
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
+                                    <div>
+                                        <x-label>Teléfono</x-label>
+                                        <x-input wire:model="phone" class="w-full" />
+                                        @error('phone')
+                                            <span>
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
+                                    <div class="flex flex-col">
+                                        <x-label>País</x-label>
+                                        <select wire:model="selectedCountry" wire:change="getCities" class="w-full">
+                                            <option value="">Selecciona un país</option>
+                                            @foreach ($countries as $country)
+                                                <option value="{{ $country }}">{{ $country }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+
+                                    <div class="flex flex-col">
+                                        <x-label>Buscar ciudad</x-label>
+                                        <input type="text" wire:model.debounce.300ms="searchCity" placeholder="Buscar ciudad"
+                                            class="w-full">
+                                    </div>
+
+                                    <div class="flex flex-col">
+                                        <x-label>Ciudad</x-label>
+                                        <select wire:model="city" class="w-full">
+                                            <option value="">Selecciona una ciudad</option>
+                                            @foreach ($cities as $city)
+                                                <option value="{{ $city }}">{{ $city }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+
+                                    <div>
+                                        <x-label>Domicilio</x-label>
+                                        <x-input wire:model="home" class="w-full" />
+                                        @error('home')
+                                            <span>
+                                                {{ $message }}
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </x-slot>
+
+                            <x-slot name="footer">
+                                <button wire:click="{{ !$client_id ? 'addClient' : 'updateClient' }}"
+                                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Guardar</button>
+                                <button wire:click="closeModal"
+                                    class="bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded ml-4 flex items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                        stroke="currentColor" class="w-6 h-6 mr-2">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                    </svg>
+                                    Cancelar
+                                </button>
+                            </x-slot>
+                        </x-dialog-modal>
+
+                        <x-dialog-modal wire:model="confirmDeleteClientModal">
+                            <x-slot name="title">Confirmar eliminación</x-slot>
+                            <x-slot name="content">
+                                ¿Estás seguro de que quieres eliminar este cliente?
+                            </x-slot>
+                            <x-slot name="footer">
+                                <button wire:click="deleteClient({{ $client_id }})"
+                                    class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded flex items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                        stroke="currentColor" class="w-6 h-6 mr-2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                                    </svg>
+                                    Sí
+                                </button>
+                                <button wire:click="$set('confirmDeleteClientModal', false)"
+                                    class="bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded ml-4 flex items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                        stroke="currentColor" class="w-6 h-6 mr-2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                    </svg>
+                                    No
+                                </button>
+                            </x-slot>
+                        </x-dialog-modal>
+
         </div>
     </div>
-@endsection
+</div>
+
