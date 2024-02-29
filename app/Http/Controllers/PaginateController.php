4df9d1a1c2f4c5469df27d92e73a25c3123d35c2
JoseEmiliano01 <2130066@itace.edu.mx>
2024-02-29 16:40:49 -0600

    test

--- app/Http/Controllers/PaginateController.php
+++ app/Http/Controllers/PaginateController.php
@@ -12,13 +12,12 @@ class PaginateController extends Controller
         try {
             $services = Service::paginate(5);
             return view('servicios', compact('services'));
-        } catch (\Illuminate\Database\QueryException $e) {
-            return response()->view('error', ['error' => 'Error de la base de datos: ' . $e->getMessage()], 500);
         } catch (\Exception $e) {
             return response()->view('error', ['error' => $e->getMessage()], 500);
         }
     }
 }
+
 
 //Versiones de public functions antiguas, en dicho caso de que el actual "Try-catch" no funcione. Es una especie de respaldo.
 
