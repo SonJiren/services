63e94d265b64da19fb67c7e896ab8245d79efccf
JoseEmiliano01 <2130066@itace.edu.mx>
2024-02-29 16:51:40 -0600

    Deleted unnecesary commentary

--- app/Http/Controllers/PaginateController.php
+++ app/Http/Controllers/PaginateController.php
@@ -12,34 +12,10 @@ class PaginateController extends Controller
         try {
             $services = Service::paginate(5);
             return view('servicios', compact('services'));
+        } catch (\Illuminate\Database\QueryException $e) {
+            return response()->view('error', ['error' => 'Error de la base de datos: ' . $e->getMessage()], 500);
         } catch (\Exception $e) {
             return response()->view('error', ['error' => $e->getMessage()], 500);
         }
     }
 }
-
-
-//Versiones de public functions antiguas, en dicho caso de que el actual "Try-catch" no funcione. Es una especie de respaldo.
-
-/*
-{
-    public function index()
-    {
-        try {
-            $services = Service::paginate(5);
-            return view('servicios', compact('services'));
-        } catch (\Exception $e) {
-            return response()->view('error', ['error' => $e->getMessage()], 500);
-        }
-    }
-}
-
-class PaginateController extends Controller
-{
-    public function index()
-    {
-        $services = Service::paginate(5);
-        return view('servicios', compact('services'));
-    }
-}
-*/
