6435247f47bc85d6829b0113713fed5de5e61d31
JoseEmiliano01 <2130066@itace.edu.mx>
2024-03-07 17:13:23 -0600

    PaginateControlled rewrite

--- app/Http/Controllers/PaginateController.php
+++ app/Http/Controllers/PaginateController.php
@@ -7,6 +7,23 @@ use App\Models\Service;
 
 class PaginateController extends Controller
 {
+    public function index(Request $request)
+    {
+        try {
+            $services = Service::paginate(5);
+            $servicesLinks = $services->links();
+            return view('servicios', compact('services', 'servicesLinks'));
+        } catch (\Illuminate\Database\QueryException $e) {
+            return response()->withException($request, $e);
+        } catch (\Exception $e) {
+            return response()->withException($request, $e);
+        }
+    }
+}
+
+
+
+/* {
     public function index()
     {
         try {
@@ -19,20 +36,4 @@ class PaginateController extends Controller
             return response()->view('error', ['error' => $e->getMessage()], 500);
         }
     }
-}
-
-
-/*
-public function index()
-{
-    try {
-        $services = Service::paginate(5);
-        return view('servicios', compact('services'));
-    } catch (\Illuminate\Database\QueryException $e) {
-        return response()->view('error', ['error' => 'Error de la base de datos: ' . $e->getMessage()], 500);
-    } catch (\Exception $e) {
-        return response()->view('error', ['error' => $e->getMessage()], 500);
-    }
-}
-}
-*/
+} */
