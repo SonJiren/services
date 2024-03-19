7bf43c346865221799a592d78d6f5581e278d445
JoseEmiliano01 <2130066@itace.edu.mx>
2024-03-19 11:56:54 -0600

    commented new clientservice component

--- app/Payments/PaymentGateway.php
+++ app/Payments/PaymentGateway.php
@@ -18,3 +18,33 @@ class PaymentGateway
         return true; //Pago exitoso
     }
 }
+
+/*
+public function updateClientService()
+    {
+        $this->validate([
+            'client_id' => 'required',
+            'job_id' => 'required',
+            'cost' => 'required',
+            'country' => 'required',
+            'city' => 'required',
+            'address' => 'required',
+            'date' => 'required',
+        ]);
+
+        $clientservice = ClientService::findOrFail($this->clientservice_id);
+        $clientservice->update([
+            'client_id' => $this->client_id,
+            'job_id' => $this->job_id,
+            'cost' => $this->cost,
+            'country' => $this->selectedCountry,
+            'city' => $this->city,
+            'address' => $this->address,
+            'date' => $this->date,
+        ]);
+        Notification::route('mail', 'liderthragg@gmail.com')
+        ->notify(new PaymentNotification($clientservice->job_id, $clientservice->cost));
+
+        $this->closeModal();
+    }
+*/
